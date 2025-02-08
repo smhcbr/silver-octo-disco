9830be048994d2ba76dae79ed01331499e5dc5dd
smh_cbr <smh_cbr@hotmail.com>
2025-02-08 18:50:28 +0300

    salesByMatch is solved.

---
+++ hackerrank/monthly_challenge/week2/salesByMatch/Solution.java
@@ -0,0 +1,52 @@
+package week2.salesByMatch;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        Set<Integer> intSet = new HashSet<>(ar);
+
+        int count = 0;
+
+        for(Integer i: intSet){
+
+            count += Math.floorDiv(Collections.frequency(ar, i),2);
+        }
+
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
