8856eaf6504f73e709fd3984de7c95083f765ac0
smh_cbr <smh_cbr@hotmail.com>
2025-02-08 13:02:11 +0300

    exorString 2 is solved.

---
+++ hackerrank/monthly_challenge/xorStrings2/Solution.java
@@ -0,0 +1,31 @@
+package xorStrings2;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static String xorStrings(String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        int len = s1.length();
+        for (int i = 0; i < len; i++) {
+            if(s1.charAt(i) == s2.charAt(i))
+                sb.append("0");
+            else
+                sb.append("1");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+
+        String xor = xorStrings(s1, s2);
+
+        System.out.println(xor);
+        scanner.close();
+    }
+}
